688e9b09dad0dd42b976f0d8252f8660b4022ce3
João Lucas <joao.lsilva1198@gmail.com>
2021-04-14 12:00:30 -0300

    Game Update, README update and .gitignore

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Jogo da Velha
-# Implementação do Jogo da Velha em Python
+# Jogo da Velha
+## Implementação do Jogo da Velha em Python
 
 #Status
- [] Em desenvolvimento
+ * Em desenvolvimento
--- jogoDaForca.py
+++ jogoDaForca.py
@@ -1,14 +1,37 @@
-palavras = ['Banana', 'Abacate']
-espaco = []
-print('Vamos jogar')
+def jogo():
+    palavras = ['Banana', 'Abacate']
+    palavra_digitada = ""
+    espaco = []
+    corpo = 0
+# 0
+#/|\
+# |
+#/-\
+    print('A palavra tem ', len(palavras[0]), 'letras')
+    for i in range(len(palavras[0])):
+        espaco.append("_")
+        print(espaco[i],end=" ")
+    print()
 
-print('A palavra tem ',len(palavras[0]), 'letras')
-for i in range(len(palavras[0])):
-    espaco.append("_")
-    print(espaco[i],end=" ")
-print()
-letra = input('Digite uma letra: ')
 
-if letra in palavras[0]:
-    print('acertou')
+    while palavra_digitada != palavras[0] or corpo < 8:
+        letra = input('Digite uma letra: ')
+        if letra in palavras[0]:
+            print('acertou')
+            palavra_digitada = letra
+        else:
+            print('Essa letra não está na palavra')
+            corpo += 1
     
+def desenho_forca():
+    print('-----')
+    for i in range(4):
+        if i == 0:
+            print('|    |')
+        else:
+            print('|')
+
+if __name__ == '__main__':
+    print('Vamos jogar')
+    desenho_forca()
+    jogo()
