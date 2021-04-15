b12c10f00d6be5c1f193774190b88b1941333f9b
João Lucas <joao.lsilva1198@gmail.com>
2021-04-15 15:29:38 -0300

    Game version 1

--- jogoDaForca.py
+++ jogoDaForca.py
@@ -1,33 +1,58 @@
-def jogo():
-    palavra_digitada = ""
-    espaco = []
-    corpo = 0
+import palavras
 # 0
 #/|\
-# |
 #/-\
-    print('A palavra tem ', len(palavras[0]), 'letras')
-    for i in range(len(palavras[0])):
-        espaco.append("_")
-        print(espaco[i],end=" ")
-    print()
-
-
-    while palavra_digitada != palavras[0] or corpo < 8:
-        letra = input('Digite uma letra: ')
-        if letra.upper() in palavras[0]:
-            print('acertou')
-            palavra_digitada = letra
-        else:
-            print('Essa letra não está na palavra')
-            corpo += 1
-    
 def desenho_forca():
     print('-----')
     i =1
     print('|    |')
     for i in range(4):
         print('|')
+        
+def imprime_espaco(palavra_digitada):
+     for i in range(len(palavra_digitada)):
+        print(palavra_digitada[i],end=" ")
+     print()
+    
+def jogo():
+    p = palavras.Palavras()
+    palavra_secreta = p.lista_palavras()
+    tamanho_palavra = len(palavra_secreta)
+    palavra_digitada = []
+    acertou = errou = False
+    corpo = 0
+
+    for i in range(tamanho_palavra):
+        palavra_digitada.append("_")
+    print('A palavra tem ', tamanho_palavra, 'letras')
+    imprime_espaco(palavra_digitada)
+
+
+    while not acertou and not errou:
+        chute = input('Digite uma letra: ')
+        if chute.upper() in palavra_secreta:
+            print('acertou')
+            posicao = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra:
+                    palavra_digitada[posicao] = letra
+                posicao += 1
+            
+        else:
+            print('Essa letra não está na palavra')
+            corpo+= 1
+        imprime_espaco(palavra_digitada)
+
+        errou = corpo == 7
+        acertou = '_' not in palavra_digitada
+
+    if acertou:
+        print('Parabéns, você descobriu a palvra!')
+    else:
+        print('Você não descobriu a palavra, mais sorte na próxima!')
+        
+    
+
 
 if __name__ == '__main__':
     print('Vamos jogar')
--- palavras.py
+++ palavras.py
@@ -1,10 +1,10 @@
 import random
 
 class Palavras:
-    def lista_palavras():
+    def lista_palavras(self):
         palavras = ['ABACATE','BANANA', 'LARANJA', 'MELANCIA', 'UVA', 'GOIABA']
         tamanho = len(palavras)
-        indice = random.randeint(0, tamanho)
+        indice = random.randint(0, tamanho-1)
         return palavras[indice]
         
         
